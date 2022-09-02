d5dbc270d366cd05b89fc1dbc64ec900497db499
Jiraphon Yenphraphai <jyenphraphai@gmail.com>
2022-09-02 17:35:59 -0400

    q 2

--- leetcode/1/two_sum_hash.cpp
+++ leetcode/1/two_sum_hash.cpp
@@ -1,27 +1,29 @@
-// overloading operators example
 #include <iostream>
+#include <iterator>
+#include <map>
 #include <vector>
 using namespace std;
 
 class Solution {
 public:
   vector<int> twoSum(vector<int>& nums, int target) {
-    vector<int> out;
-    for (int i=0; i != nums.size(); i++) {
-      for (int j=i+1; j != nums.size(); j++) {
-        if (nums[i] + nums[j] == target) {
-          out.push_back(i);
-          out.push_back(j);
-          return out;
-        }
-      }
-    }
+    map<int, int> hash_table;
+    vector <int> out;
+    for (int i = 0; i != nums.size(); i++){
+      if (hash_table.find(target - nums[i]) != hash_table.end()) {
+        out.push_back(hash_table[target - nums[i]]);
+        out.push_back(i);
+      } 
+      else 
+        hash_table[nums[i]] = i;
+    } 
     return out;
   }
 };
 
-int main () {
-  vector<int> g1;
+int main()
+{
+vector<int> g1;
   g1.push_back(3);
   g1.push_back(2);
   g1.push_back(4);
