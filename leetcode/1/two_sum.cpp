ab5b285f0eff216e044220ce11257e8219d3ff7d
Jiraphon Yenphraphai <jyenphraphai@gmail.com>
2022-09-02 11:22:39 -0400

    day 1

---
+++ leetcode/1/two_sum.cpp
@@ -0,0 +1,38 @@
+// overloading operators example
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  vector<int> twoSum(vector<int>& nums, int target) {
+    int size=nums.size();
+    vector<int> out;
+    for (int i=0; i != size; i++) {
+      out.push_back(i);
+      for (int j=i+1; j != size; j++) {
+        if (nums[i] + nums[j] == target) {
+          out.push_back(j);
+          return out;
+        }
+      }
+      out.clear();
+      
+    }
+    return out;
+  }
+};
+
+int main () {
+  vector<int> g1;
+  g1.push_back(3);
+  g1.push_back(2);
+  g1.push_back(4);
+  Solution sol;
+  vector<int> out;
+  out = sol.twoSum(g1, 6);
+  for (auto i=out.begin(); i != out.end(); i++) {
+    cout << *i << endl;
+  }
+  return 0;
+}
