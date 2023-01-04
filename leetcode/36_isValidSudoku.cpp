ba2480aea2eb1bb371cd39fd8898725dde048e9e
Jiraphon Yenphraphai <jyenphraphai@gmail.com>
2023-01-04 00:09:45 -0500

    day 53

--- leetcode/36_isValidSudoku.cpp
+++ leetcode/36_isValidSudoku.cpp
@@ -1,106 +1,38 @@
-#include <iostream>
-#include <iterator>
-#include <map>
-#include <vector>
-#include <queue>
-#include <cmath>
-#include <set>
-#include <unordered_set>
-#include <algorithm>
+/* 
+36. Valid Sudoku
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
 
-using namespace std;
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
 
-class SolutionNaive {
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+*/
+class Solution {
 public:
   bool isValidSudoku(vector<vector<char>>& board) {
-    return checkCol(board) && checkRow(board) && checkSub(board);
-  }
+    int nums = 9;
+    int rows[nums][nums];
+    int cols[nums][nums];
+    int boxes[nums][nums];
+    memset(rows, 0, sizeof(rows));
+    memset(cols, 0, sizeof(cols));
+    memset(boxes, 0, sizeof(boxes));
 
-private:
-  bool checkRow(const vector<vector<char>>& board){
-    for (int i=0; i < board.size(); i++){
-      unordered_set<int> S;
-      for (int j=0; j < board[0].size(); j++){
-        if (board[i][j] == '.') {continue;}
-        if (S.find(board[i][j]) != S.end()){return false;}
-        else {S.insert(board[i][j]);}
-      }
-    }
-    return true;
-  }
-
-  bool checkCol(const vector<vector<char>>& board){
-    for (int j=0; j < board[0].size(); j++){
-      unordered_set<int> S;
-      for (int i=0; i < board.size(); i++){
-        if (board[i][j] == '.') {continue;}
-        if (S.find(board[i][j]) != S.end()){return false;}
-        else {S.insert(board[i][j]);}
-      }
-    }
-    return true;
-  }
-  
-  bool checkSub(const vector<vector<char>>& board){
-    for (int i=1; i < board.size(); i += 3){
-      for (int j=1; j < board[0].size(); j += 3){
-        unordered_set<int> S;
-        //cout << i<<",  " << j<< endl;
-        for (int it1=-1; it1 !=2; it1++){
-          for (int it2=-1; it2 !=2; it2++){
-            
-            if (board[i + it1][j + it2] == '.') {continue;}
-            if (S.find(board[i + it1][j + it2]) != S.end()){return false;}
-            else {S.insert(board[i + it1][j + it2]);}
-          }
+    for (int i=0; i<nums; i++){
+      for (int j=0; j<nums; j++){
+        if (board[i][j]== '.')
+          continue;
+        int idx = (int) j / 3 + 3 * (i / 3);
+        int cur = board[i][j] - '1';
+        rows[i][cur]++; cols[j][cur]++; boxes[idx][cur]++;
+        if (rows[i][cur] != 1 || cols[j][cur] != 1 || boxes[idx][cur] != 1){
+          return false;
         }
       }
     }
-
     return true;
   }
 };
-
-
-class Solution{
-public:
-  bool isValidSudoku(vector<vector<char>>& board) {
-    for (int i=0; i < board.size(); i++){
-      unordered_set<int> setRow, setCol, setgrid;
-      for (int j=0; j < board[0].size(); j++){
-        if (board[i][j] == '.') {continue;}
-        if (setRow.find(board[i][j]) != setRow.end()){return false;}
-        else {setRow.insert(board[i][j]);}
-        
-      }
-    }
-  }
-};
-
-int main()
-{ 
-  
-  Solution* sol;
-  vector<vector<char>> board = {{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
-                                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
-                                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
-                                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
-                                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
-                                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
-                                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
-                                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
-                                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
-  cout << sol ->isValidSudoku(board)<<endl;
-
-  board = vector<vector<char>> {{'8', '3', '.', '.', '7', '.', '.', '.', '.'},
-                                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
-                                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
-                                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
-                                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
-                                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
-                                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
-                                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
-                                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
-  cout << sol ->isValidSudoku(board)<<endl;
-  return 0;
-}
