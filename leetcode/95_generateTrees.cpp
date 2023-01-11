efae85a30530f53ad21e554dce874394c554d6c8
Jiraphon Yenphraphai <jyenphraphai@gmail.com>
2023-01-10 23:59:22 -0500

    day 58

--- leetcode/95_generateTrees.cpp
+++ leetcode/95_generateTrees.cpp
@@ -1,53 +1,46 @@
-#include <iostream>
-#include <iterator>
-#include <map>
-#include <vector>
-#include <tuple>
-
-using namespace std;
-
-struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode() : val(0), left(nullptr), right(nullptr) {}
-  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
+/*
+95. Unique Binary Search Trees II
+Given an integer n, return all the structurally unique BST's (binary search trees), which has exactly n nodes of unique values from 1 to n. Return the answer in any order.
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
   vector<TreeNode*> generateTrees(int n) {
-    vector<vector<TreeNode*>> tree;
-    tree.push_back(vector<TreeNode*> {nullptr});
+    vector<TreeNode*> res;
+    res = helper(1, n);
+    return res;
+  }
+private:
+  vector<TreeNode*> helper(int start, int end){
+    if (start > end)
+      return vector<TreeNode*> {nullptr};
 
-    TreeNode* cur = new TreeNode(1);
-    tree.push_back(vector<TreeNode*> {cur});
-
-    for (int i=2; i <= n; i++){
-      vector<TreeNode*> cur;
-      for (int j=0; j < i; j++){
-        for (int k=0; k < tree[i - j - 1].size(); k++){
-          for (int l=0; l < tree[j].size(); l++){
-            TreeNode* head = new TreeNode(i);
-            head -> left = tree[i - j - 1][k];
-            head -> right = tree[j][l];
-            cur.push_back(head);
-          }
+    vector<TreeNode*> res;
+    for (int i=start; i<= end; i++){
+      vector<TreeNode*> leftSub = helper(start, i-1);
+      vector<TreeNode*> rightSub = helper(i+1, end);
+      
+      for (auto l: leftSub){
+        for (auto r: rightSub){
+          TreeNode* cur = new TreeNode(i);
+          cur -> left =l;
+          cur -> right = r;
+          res.push_back(cur);
         }
       }
-      tree.push_back(cur);
+      
+      
     }
-    return tree[n];
+    return res;
   }
 };
-
-int main()
-{ 
-  
-  Solution* sol;
-
-  sol -> generateTrees(2);
-
-  return 0;
-}
