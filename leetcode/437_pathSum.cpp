8a6d6409e1cea8d9f87a0b43db8363d767c771a5
Jiraphon Yenphraphai <jyenphraphai@gmail.com>
2023-01-13 23:53:41 -0500

    day 61

--- leetcode/437_pathSum.cpp
+++ leetcode/437_pathSum.cpp
@@ -1,89 +1,39 @@
-#include <iostream>
-#include <iterator>
-#include <map>
-#include <set>
-#include <unordered_set>
-#include <vector>
-#include <tuple>
+/*
+437. Path Sum III
+Given the root of a binary tree and an integer targetSum, return the number of paths where the sum of the values along the path equals targetSum.
 
-using namespace std;
-
-struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode() : val(0), left(nullptr), right(nullptr) {}
-  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-void print(vector<vector<int>> x){
-  for (auto row: x){
-    for (auto elem: row){
-      cout << elem <<", ";}
-    cout << endl;
-  }
-}
-
+The path does not need to start or end at the root or a leaf, but it must go downwards (i.e., traveling only from parent nodes to child nodes).
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
+  unordered_map<long, int> map;
 public:
   int pathSum(TreeNode* root, int targetSum) {
-    if (!root){return 0;}
-    int count(0);
-    vector<TreeNode*> cur;
-    DFS(root, targetSum, count, 0);
-    
-    return count;
+    int ans=0;
+    map[0] = 1;
+    helper(root, targetSum, 0, ans);
+    return ans;
   }
 private:
-  void DFS(TreeNode* root, int targetSum, int& count, int sel){
-    
-    if (!root){return;}
-    
-    if (sel == 0){
-      DFS(root -> left, targetSum, count, 0);
-      DFS(root -> right, targetSum, count, 0);
-      DFS(root, targetSum, count, 1);
-    }
-    else {
-     
-      if (targetSum == root ->val){count++; return;}
-      else {
-        DFS(root -> left, targetSum - root -> val, count, 1);
-        DFS(root -> right, targetSum - root -> val, count, 1);
-      }
-     
-    }
+  void helper(TreeNode* root, int targetSum, long sum, int& ans){
+    if (!root)
+      return;
+    sum += (long) root -> val;
+    if (map.count(sum - targetSum))
+      ans += map[sum - targetSum];
+    map[sum]++;
+    helper(root -> left, targetSum, sum, ans);
+    helper(root -> right, targetSum, sum, ans);
+    map[sum]--;
   }
 };
-
-int main()
-{ 
-  
-  Solution* sol;
-  TreeNode* node1 = new TreeNode(10);
-
-  TreeNode* node2_1 = new TreeNode(5);
-  TreeNode* node2_2 = new TreeNode(-3);
-
-  TreeNode* node3_1 = new TreeNode(3);
-  TreeNode* node3_2 = new TreeNode(2);
-  TreeNode* node3_3 = new TreeNode(11);
-
-  TreeNode* node4_1 = new TreeNode(3);
-  TreeNode* node4_2 = new TreeNode(-2);
-  TreeNode* node4_3 = new TreeNode(1);
-
-  node1 ->left = node2_1; node1 -> right = node2_2;
-  node2_1 -> left = node3_1; node2_1 -> right= node3_2;
-  node3_1 -> left = node4_1; node3_1 -> right = node4_2;
-  node2_2 -> right = node3_3; node3_2 ->right = node4_3;
-
-  cout << sol ->pathSum(node1, 8)<< endl;
-  TreeNode* node11 = new TreeNode(1);
-
-  TreeNode* node21_1 = new TreeNode(2);
-  TreeNode* node21_2 = new TreeNode(3);
-  //node11 ->left = node21_1; node21_1 -> right = node21_2;
-  cout << sol ->pathSum(node1, 0)<< endl;
-  return 0;
-}
--- leetcode/560_subarraySum.cpp
+++ leetcode/560_subarraySum.cpp
@@ -1,62 +1,24 @@
-#include <iostream>
-#include <iterator>
-#include <map>
-#include <vector>
-#include <queue>
-#include <stack>
+/*
+560. Subarray Sum Equals K
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
 
-using namespace std;
-
-void print(vector<vector<int>> x){
-  for (auto row: x){
-    for (auto elem: row){
-      cout << elem <<", ";}
-    cout << endl;
-  }
-  
-}
-
-void print(vector<int> x){
- 
-    for (auto elem: x){
-      cout << elem <<", ";}
-    cout << endl;
-
-  
-}
-
+A subarray is a contiguous non-empty sequence of elements within an array.
+*/
 class Solution {
 public:
   int subarraySum(vector<int>& nums, int k) {
-    int n = nums.size();
-    map<int, int> hashTable;
-    int count(0);
-    int sum(0);
-    for (int i=0; i != n; i++){
-      sum += nums[i];
-      if (sum == k){count++;}
-      if (hashTable.find(sum - k) != hashTable.end()){
-        count += hashTable[sum - k];
-      }
-      if (hashTable.find(sum) == hashTable.end()){hashTable[sum] = 1;}
-      else {hashTable[sum]++;}
+    int n=nums.size();
+    int count=0;
+    int prefixSum=0;
+    unordered_map<int, int> map;
+    map[0] = 1;
+    for (int i=0; i<n; i++){
+      prefixSum += nums[i];
+      if (map.count(prefixSum - k))
+        count += map[prefixSum - k];
+      map[prefixSum]++;
     }
     return count;
+    
   }
 };
-
-int main()
-{ 
-  
-  Solution* sol;
- 
-  
-  vector<int> nums = {1, 1, 1};
-  cout << sol -> subarraySum(nums, 2) << endl;
-
-
-  nums = vector<int> {1, 2, 3};
-  cout << sol -> subarraySum(nums, 3) << endl;
-  
-  return 0;
-}
