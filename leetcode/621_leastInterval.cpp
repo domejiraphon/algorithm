4b1211d695eacddd262f25f5cb3fcba448fff845
Jiraphon Yenphraphai <jyenphraphai@gmail.com>
2023-01-17 21:32:00 -0500

    day 63

--- leetcode/621_leastInterval.cpp
+++ leetcode/621_leastInterval.cpp
@@ -1,41 +1,28 @@
-#include <iostream>
-#include <iterator>
-#include <map>
-#include <vector>
-#include <set>
-#include <unordered_map>
-#include <unordered_set>
-#include <queue>
-#include <stack>
-#include <tuple>
+/*
+621. Task Scheduler
+Given a characters array tasks, representing the tasks a CPU needs to do, where each letter represents a different task. Tasks could be done in any order. Each task is done in one unit of time. For each unit of time, the CPU could complete either one task or just be idle.
 
-using namespace std;
+However, there is a non-negative integer n that represents the cooldown period between two same tasks (the same letter in the array), that is that there must be at least n units of time between any two same tasks.
 
+Return the least number of units of times that the CPU will take to finish all the given tasks.
+*/
+bool sortby(int& a, int& b){
+  return a > b;
+}
 class Solution {
 public:
   int leastInterval(vector<char>& tasks, int n) {
-   
-    unordered_set<char> Set;
-    Set.insert(tasks[0]);
-    int i(0), n(tasks.size());
-    while (i < n){
-      if ()
-    }
+    vector<int> freq(26, 0);
+    for (auto task: tasks)
+      freq[task - 'A']++;
+    sort(freq.begin(), freq.end(), sortby);
+    int fMax = freq[0];
+    int idle = (fMax - 1) * n;
+    
+    for (int i=1; i<26 && freq[i] != 0 && idle >= 0; i++){
+      idle -= min(fMax - 1, freq[i]);
+    } 
+    idle = max(idle, 0);
+    return tasks.size() + idle;
   }
 };
-int main()
-{ 
-  vector<char> tasks;
-  Solution* sol;
-
-  tasks = {'A','A','A','B','B','B'};
-  cout << sol -> leastInterval(tasks, 2) << endl;
-
-  tasks = {'A','A','A','B','B','B'};
-  cout << sol -> leastInterval(tasks, 0) << endl;
-
-  tasks = {'A','A','A','A','A','A','B','C','D','E','F','G'};
-  cout << sol -> leastInterval(tasks, 2) << endl;
-
-  return 0;
-}
