643913a6d16f56843f0517f8b3e2bad6d3b818c3
Jiraphon Yenphraphai <jyenphraphai@gmail.com>
2023-01-31 21:09:42 -0500

    day 72

--- algorithm/453_minMoves.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-  int minMoves(vector<int>& nums) {
-    sort(nums.begin(), nums.end());
-    int count(0);
-    for (auto ele: nums){count += ele - nums[0];}
-    return count;
-  }
-};
